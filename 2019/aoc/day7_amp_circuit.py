742b12f53507795e9535d1777be2a3534d488fa2
Zack Dever <zackdever@gmail.com>
2019-12-07 18:50:54 -0800

    Day 7, part 2: Allow for feedback within amplication circuit

--- 2019/aoc/day7_amp_circuit.py
+++ 2019/aoc/day7_amp_circuit.py
@@ -1,5 +1,5 @@
 import itertools
-import queue
+import multiprocessing as mp
 
 from aoc import day5_diagnostic as d5
 
@@ -10,15 +10,22 @@ def thruster_signal(code, phase_sequence):
     # and the last queue is it's input.
     msg_qs = [None for _ in phase_sequence]
     for i, setting in enumerate(phase_sequence):
-        q = queue.SimpleQueue()
+        q = mp.Queue()
         q.put(setting)
         # first input is 0, per AOC instructions
         if i == 0:
             q.put(0)
         msg_qs[i-1] = q
 
+    procs = []
     for i in range(len(phase_sequence)):
-        d5.intcode(code, msg_qs[i-1], msg_qs[i])
+        # NOTE maybe await would be better, or a mp.Pool?
+        p = mp.Process(target=d5.intcode, args=(code, msg_qs[i-1], msg_qs[i]))
+        procs.append(p)
+        p.start()
+
+    for proc in procs:
+        proc.join()
 
     return msg_qs[-1].get()
 
@@ -26,7 +33,7 @@ def thruster_signal(code, phase_sequence):
 def max_thruster_signal(code, feedback=False):
     phase_range = range(5, 10) if feedback else range(5)
     phase_seqs = itertools.permutations(phase_range)
-    # TODO run in parallel
+    # NOTE Could parallelize this if it were slow
     return max(thruster_signal(code, seq) for seq in phase_seqs)
 
 
@@ -36,5 +43,5 @@ if __name__ == '__main__':
         code = [int(x) for x in f.read().strip().split(',')]
     signal = max_thruster_signal(code, feedback=False)
     print('Highest signal, without feedback, for thrusters: {}'.format(signal))
-    # signal = max_thruster_signal(code, feedback=True)
-    # print('Highest signal, WITH feedback, for thrusters: {}'.format(signal))
+    signal = max_thruster_signal(code, feedback=True)
+    print('Highest signal, WITH feedback, for thrusters: {}'.format(signal))
