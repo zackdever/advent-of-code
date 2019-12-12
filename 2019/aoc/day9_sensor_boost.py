d3560a2bfa24b67aba505ff1ba179246ab91c12c
Zack Dever <zackdever@gmail.com>
2019-12-12 14:57:42 -0800

    Day 9, part 2: sensor boost mode

--- 2019/aoc/day9_sensor_boost.py
+++ 2019/aoc/day9_sensor_boost.py
@@ -5,10 +5,16 @@ if __name__ == '__main__':
     with open(fp) as f:
         code = [int(x) for x in f.read().strip().split(',')]
     
+    # part 1
     input_val = 1 # test mode
     output = d5.intcode_helper(code, input_val)
     if len(output) != 1:
         print('Error codes! {}'.format(output[:-1]))
     else:
         print('No errors!')
-    print('BOOST keycode: {}'.format(output[-1]))
+        print('BOOST keycode: {}'.format(output[-1]))
+
+        # part 2
+        input_val = 2 # sensor boost mode
+        output = d5.intcode_helper(code, input_val)
+        print('Distress signal coordinates: {}'.format(output))
